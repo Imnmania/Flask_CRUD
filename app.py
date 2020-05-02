d2011772a1da867f6d586b1712d4a349b5d8a71e
Imnmania <niloyb@live.com>
2020-05-02 23:54:57 +0600

    upto add employee UI

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/index')
+@app.route('/home')
+def index():
+    return render_template('/index.html', title='Home')
+
+
+@app.route('/about')
+def about():
+    return render_template('/about.html', title='About')
+
+
+@app.route('/login')
+def login():
+    return render_template('/login.html')
+
+@app.route('/register')
+def register():
+    return render_template('/register.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
