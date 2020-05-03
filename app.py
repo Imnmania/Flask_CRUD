94b7e073fee345fe9b44f8d8a1939f78268285c5
Imnmania <niloyb@live.com>
2020-05-03 21:02:37 +0600

    database insert works

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.secret_key = 'Secrert Key'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:hunter@localhost/crud'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:hunter@localhost/crud'    #dbSysName://user:password@host/dbname
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
+# This acts as table named 'data' in our database
 class Data(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(100))
@@ -18,7 +19,7 @@ class Data(db.Model):
         self.name = name
         self.email = email
         self.phone = phone
-        
+
 
 @app.route('/')
 @app.route('/index')
@@ -36,10 +37,24 @@ def about():
 def login():
     return render_template('/login.html')
 
-@app.route('/register')
+@app.route('/register', methods=['GET','POST'])
 def register():
-    return render_template('/register.html')
+    #return render_template('/register.html')
+    return "huuuu"
 
+@app.route('/insert', methods=['GET','POST'])
+def insert():
+    if request.method == 'POST':
+
+        name = request.form['name']
+        email = request.form['email']
+        phone = request.form['phone']
+
+        my_data = Data(name, email, phone)
+        db.session.add(my_data)
+        db.session.commit()
+
+        return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
